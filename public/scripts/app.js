c4267c51d27ff3257526d6b68cb37660e2898aba
Nikko Lee <nlee@C02SGG6KG8WP.corp.proofpoint.com>
2017-10-16 17:30:31 -0700

    Search function working.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
+	 // sorting table
 	 $('table').tablesort();
+
+	$(function () {
+    $( '#table' ).searchable({
+        striped: true,
+        oddRow: { 'background-color': '#f5f5f5' },
+        evenRow: { 'background-color': '#fff' },
+        searchType: 'fuzzy'
+    });
+    
+    $( '#searchable-container' ).searchable({
+        searchField: '#container-search',
+        selector: '.row',
+        childSelector: '.col-xs-4',
+        show: function( elem ) {
+            elem.slideDown(100);
+        },
+        hide: function( elem ) {
+            elem.slideUp( 100 );
+        }
+    })
+});
+
 });
 
 /*
